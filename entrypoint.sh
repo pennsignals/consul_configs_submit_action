0bd881561f73ff6f69084e05e80353bb22b2e1b3
Darryl Mendillo <1238655+darrylmendillo@users.noreply.github.com>
2020-11-27 16:42:25 -0500

    print variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# echo all the variabls
+echo "INPUT_ADDR: $INPUT_ADDR"
+echo "INPUT_PATH: $INPUT_PATH"
+echo "INPUT_REGEX: $INPUT_REGEX"
+echo "INPUT_LOCATION: $INPUT_LOCATION"
+
 # find all files in subdirectories of location
 CONFIG_FILES=$(find "$INPUT_LOCATION" -type f -regextype posix-extended -regex "$INPUT_REGEX")
 
