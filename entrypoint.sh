54223316565d0074ff87056e44886b7055758b72
Darryl Mendillo <1238655+darrylmendillo@users.noreply.github.com>
2020-11-27 17:41:50 -0500

    update syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ echo "INPUT_REGEX: $INPUT_REGEX"
 echo "INPUT_LOCATION: $INPUT_LOCATION"
 
 # find all files in subdirectories of location
-CONFIG_FILES=$(find "$INPUT_LOCATION" -type f -regextype posix-extended -regex "$INPUT_REGEX")
+CONFIG_FILES=$(find "${INPUT_LOCATION}" -type f -regextype posix-extended -regex "${INPUT_REGEX}")
 
 /scripts/submit_to_consul.sh --path "${INPUT_PATH}" --address "${INPUT_ADDR}" "${CONFIG_FILES}"
