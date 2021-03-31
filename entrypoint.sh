d9c2e0afe6a271ff34a3c9eba55df24944a64802
Darryl Mendillo <1238655+darrylmendillo@users.noreply.github.com>
2021-03-31 14:12:52 -0400

    use consul CLI instead of API

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,26 @@
 #!/bin/sh
 
 # echo all the variabls
-echo "CONSUL_ADDR: $CONSUL_ADDR"
+echo "CONSUL_HTTP_ADDR: $CONSUL_HTTP_ADDR"
 echo "DEPLOY_CONFIG: $DEPLOY_CONFIG"
 echo "ENV: $ENV"
 
-# parse DEPLOY_CONFIG file and build VAULT_PATH
-
-# Use input environment parameter if given
-CONFIG_ENV=$(yq read $DEPLOY_CONFIG 'deploy')
-DEPLOY="$ENV:-$CONFIG_ENV"
-
-CONSUL_PATH=$(yq read $DEPLOY_CONFIG 'organization')/$(yq read $DEPLOY_CONFIG 'project')
+# parse DEPLOY_CONFIG file and build PATH
+ORGANIZATION=$(yq read $DEPLOY_CONFIG 'organization')
+PROJECT=$(yq read $DEPLOY_CONFIG 'project')
 SERVICES=$(yq read --printMode p $DEPLOY_CONFIG 'services.*.' | cut -f2 -d '.')
-REGEX=$(yq read $DEPLOY_CONFIG 'template.configs.regex')
 
 # go through each SERVICE
 for SERVICE in $(echo $SERVICES | sed "s/,/ /g"); do
     echo "Service: $SERVICE"
 
-    # build CONSUL PATH
-    CONFIGS_PATH=$(yq read $DEPLOY_CONFIG services.$SERVICE.location)/$(yq read $DEPLOY_CONFIG template.configs.location)
+    # get file based on environenment (staging / production)
+    CONFIG_FILE=$(yq read $DEPLOY_CONFIG services.$SERVICE.location)/$(yq read $DEPLOY_CONFIG template.configs.location)/$(yq read $DEPLOY_CONFIG services.$SERVICE.configuration.local.$ENV)
+    
+    # set filename to .configuration.name
+    FILE_NAME=$(yq read $DEPLOY_CONFIG services.$SERVICE.configuration.name)
 
-    # find all config files matching regex file extension and deploy environement (example staging.yml)
-    CONFIG_FILES=$(find "${CONFIGS_PATH}" -type f -regextype posix-extended -regex "${REGEX}" -and -regex ".*.${DEPLOY}.*")
-
-    # submit all found config files to consul
-    echo "/scripts/submit_to_consul.sh --path ${CONSUL_PATH} --address ${CONSUL_ADDR} --deploy ${DEPLOY} --service "${SERVICE}" ${CONFIG_FILES}"
-    /scripts/submit_to_consul.sh --path "${CONSUL_PATH}" --address "${CONSUL_ADDR}" --deploy "${DEPLOY}" --service "${SERVICE}" "${CONFIG_FILES}"
+    # submit to consul
+    consul kv put $ORGANIZATION/$PROJECT/$SERVICE/$FILE_NAME $CONFIG_FILE
 
 done
